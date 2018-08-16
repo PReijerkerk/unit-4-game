ab248e589d0d7fd4b6ab5ab1bf6d8a04fd5f5525
P Reijerkerk <preijerkerk@gmail.com>
2018-08-16 00:16:31 -0400

    Created variables and functions to generate both the random# and randomcrystal values

---
+++ assets/javascript/game.js
@@ -0,0 +1,52 @@
+//document ready function
+$(document).ready(function () {
+//Variables
+var userWins = 0;
+var userLosses = 0;
+var userTotalScore = 0;
+var blue = 0;
+var yellow = 0;
+var green = 0;
+var red = 0;
+var computerNum = 0;
+
+//displays initial score to 0
+$("#score").html(userTotalScore);
+
+//Generate a random # for the user
+const generateRandomNum = function(){
+    var randomScore = Math.floor(Math.random() * 100) + 19;
+    computerNum = randomScore;
+    console.log(randomScore);
+}
+
+//display random number to html
+//BUG // Calls the generateRandomNum function, but does not change the html of the span id randomNumber
+$("#randomNumber").html(generateRandomNum());
+
+//base function for generating a value for crystals
+const generateCrystalNum = function() {
+    var randomCrystal = Math.floor(Math.random() * 12) + 1;
+    return randomCrystal;
+}
+
+//Generates the random numbers for the crystals, and assigns the values to the variables
+const initialCrystalNums = function() {
+    blue = generateCrystalNum();
+    yellow = generateCrystalNum();
+    green = generateCrystalNum();
+    red = generateCrystalNum();
+}
+
+//Calls the crystalNum function that assigns values to the color variables
+//logs the color variables for testing purposes
+initialCrystalNums();
+console.log(blue);
+console.log(yellow);
+console.log(green);
+console.log(red);
+});
+
+
+
+
